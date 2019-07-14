1b7d51521005a6480857714d82c682f23a69bdb8
Nicholas Dunlap <pjnick-300@hotmail.com>
2019-07-14 07:45:37 +0000

    Automatically backed up by Learn

--- spec/jukebox_spec.rb
+++ spec/jukebox_spec.rb
@@ -76,13 +76,13 @@ describe "CLI_Jukebox" do
       help_output = capture_stdout { run(songs) }
       expect(help_output).to include("help").and include("list").and include("play").and include("exit").and include("Please enter a command:").and include("Goodbye")
     end
-=begin
     it "responds to 'list'" do
       allow(self).to receive(:gets).and_return("list", "exit")
       list_output = capture_stdout { run(songs) }
       expect(list_output).to include("Please enter a command:").and include("1. Phoenix - 1901").and include("9. Amos Lee - Keep It Loose, Keep It Tight")
     end
 
+=begin
     it "responds to 'play'" do
       allow(self).to receive(:gets).and_return("play", "1", "exit")
       list_output = capture_stdout { run(songs) }
